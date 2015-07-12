9dac5e1738360e5997c8f242e68854b1e58b64e8
Richard Brauchle <swtester1@yahoo.com>
2015-07-12 14:11:43 -0400

    Simplify; no need for unlist

--- plot1.R
+++ plot1.R
@@ -15,8 +15,7 @@ filtered <- data[data["Date"] == "1/2/2007" | data["Date"] == "2/2/2007",]
 
 png("plot1.png", width=480, height=480)
 
-gapData <- unlist(filtered["Global_active_power"])
-hist(gapData, col="red", xlab="Global Active Power (kilowatts)", 
+hist(filtered$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", 
      main="Global Active Power")
 
 dev.off()
